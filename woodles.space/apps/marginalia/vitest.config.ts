09c1756b86deceb2998735de882918c750906ea9
z <brianna.derevjanik@gmail.com>
2026-06-23 13:46:52 -0400

    Fix Vitest localStorage setup

--- woodles.space/apps/marginalia/vitest.config.ts
+++ woodles.space/apps/marginalia/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		environment: 'happy-dom',
-		include: ['src/**/*.test.ts']
+		include: ['src/**/*.test.ts'],
+		setupFiles: ['../../vitest.setup.ts']
 	}
 });
